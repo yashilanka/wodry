a94b3b51c0a027349bc17068154267c92c706502
Sergey Golovin <golovim@gmail.com>
2014-07-31 11:19:23 +0400

    Now it works :)

--- build/wodry.js
+++ build/wodry.js
@@ -1,28 +1,71 @@
 (function() {
-  (function($) {
-    return $.fn.wodry = function(config) {
-      var Wodry;
-      return Wodry = (function() {
-        function Wodry(options) {
-          if (options == null) {
-            options = {
-              separator: '|',
-              delay: 2000,
-              animationTime: 500,
-              amr: 100
-            };
+  var $;
+
+  $ = jQuery;
+
+  $.fn.extend({
+    wodry: function(config) {
+      var settings;
+      if (config == null) {
+        config = {
+          separator: '|',
+          delay: 2000,
+          animationTime: 500,
+          arm: 100,
+          callback: function() {}
+        };
+      }
+      settings = $.extend({}, config);
+      this.each(function() {
+        var array, flip, flip_container;
+        flip_container = $(this);
+        array = [];
+        $.each(flip_container.text().split(settings.separator), function(key, value) {
+          array.push(value);
+        });
+        flip_container.text(array[0]);
+        flip = function() {
+          var back_text_index, front_text;
+          if (flip_container.find(".back-face").length > 0) {
+            flip_container.html(flip_container.find(".back-face").html());
           }
-          this.options = options;
-        }
-
-        Wodry.config = $.extend({}, Wodry.options);
-
-        Wodry.default_css = "style='-webkit-transform: translate3d(0,0," + Wodry.config.arm + "px); -moz-transform: translate3d(0,0," + Wodry.config.arm + "px); -ms-transform: translate3d(0,0," + Wodry.config.arm + "px); -0-transform: translate3d(0,0," + Wodry.config.arm + "px); transform: translate3d(0,0," + Wodry.config.arm + "px);";
-
-        return Wodry;
-
-      })();
-    };
-  })(jQuery);
+          front_text = flip_container.text();
+          back_text_index = $.inArray(front_text, array);
+          if ((back_text_index + 1) === array.length) {
+            back_text_index = -1;
+          }
+          flip_container.html("");
+          $("<span class='front-face'>" + front_text + "</span>").appendTo(flip_container);
+          $(".front-face").css({
+            "-webkit-transform": "translate3d(0,0," + settings.arm + "px)",
+            "-moz-transform": "translate3d(0,0," + settings.arm + "px)",
+            "-o-transform": "translate3d(0,0," + settings.arm + "px)",
+            "transform": "translate3d(0,0," + settings.arm + "px)"
+          });
+          $("<span class='back-face'>" + array[back_text_index + 1] + "</span>").appendTo(flip_container);
+          $(".back-face").css({
+            "-webkit-transform": "translate3d(0,0," + settings.arm + "px) rotateY(180deg)",
+            "-moz-transform": "translate3d(0,0," + settings.arm + "px) rotateY(180deg)",
+            "-o-transform": "translate3d(0,0," + settings.arm + "px) rotateY(180deg)",
+            "transform": "translate3d(0,0," + settings.arm + "px) rotateY(180deg)"
+          });
+          flip_container.wrapInner("<span class='adjecting' />").find(".adjecting").hide().show().css({
+            "-webkit-transform": " rotateY(180deg)",
+            "-moz-transform": " rotateY(180deg)",
+            "-o-transform": " rotateY(180deg)",
+            "transform": " rotateY(180deg)",
+            "-webkit-transition": " " + settings.animationTime + "ms",
+            "-moz-transition": " " + settings.animationTime + "ms",
+            "-o-transition": " " + settings.animationTime + "ms",
+            "transition": " " + settings.animationTime + "ms"
+          });
+        };
+        setInterval(function() {
+          flip();
+          settings.callback();
+        }, settings.delay + settings.animationTime);
+      });
+    }
+  });
 
 }).call(this);
--- src/wodry.coffee
+++ src/wodry.coffee
@@ -1,18 +1,63 @@
-(($) ->
-	$.fn.wodry = (config) ->
-		class Wodry
-			constructor: (options = {separator: '|', delay: 2000, animationTime: 500, amr: 100}) ->
-				@options = options
+$ = jQuery
 
-			@config = $.extend({}, @options)
+$.fn.extend
+	wodry : (config = {separator: '|', delay: 2000, animationTime: 500, arm: 100, callback: ->}) ->
+		settings = $.extend({}, config)
 
-			@default_css = 
-			"
-				style='-webkit-transform: translate3d(0,0,#{@config.arm}px);
-				-moz-transform: translate3d(0,0,#{@config.arm}px);
-				-ms-transform: translate3d(0,0,#{@config.arm}px);
-				-0-transform: translate3d(0,0,#{@config.arm}px);
-				transform: translate3d(0,0,#{@config.arm}px);
-			"
+		@each ->
+			flip_container = $(this)
+			array = []
+			$.each(flip_container.text().split(settings.separator), (key, value) -> 
+				array.push value
+				return)
+			flip_container.text array[0]
 
-)(jQuery);
+			flip = ->
+				if flip_container.find(".back-face").length > 0
+					flip_container.html do flip_container.find(".back-face").html
+
+				front_text = do flip_container.text
+				back_text_index = $.inArray front_text, array
+				if (back_text_index + 1) is array.length
+					back_text_index = -1
+
+				flip_container.html ""
+				$ "<span class='front-face'>#{front_text}</span>"
+					.appendTo flip_container
+				$ ".front-face"
+					.css {
+						"-webkit-transform": "translate3d(0,0,#{settings.arm}px)",
+						"-moz-transform": "translate3d(0,0,#{settings.arm}px)",
+						"-o-transform": "translate3d(0,0,#{settings.arm}px)",
+						"transform": "translate3d(0,0,#{settings.arm}px)",
+					}
+				$ "<span class='back-face'>#{array[back_text_index + 1]}</span>"
+					.appendTo flip_container
+				$ ".back-face"
+					.css {
+						"-webkit-transform": "translate3d(0,0,#{settings.arm}px) rotateY(180deg)",
+						"-moz-transform": "translate3d(0,0,#{settings.arm}px) rotateY(180deg)",
+						"-o-transform": "translate3d(0,0,#{settings.arm}px) rotateY(180deg)",
+						"transform": "translate3d(0,0,#{settings.arm}px) rotateY(180deg)",
+					}
+
+				flip_container.wrapInner "<span class='adjecting' />"
+					.find(".adjecting").hide().show().css {
+									"-webkit-transform": " rotateY(180deg)",
+									"-moz-transform": " rotateY(180deg)",
+									"-o-transform": " rotateY(180deg)",
+									"transform": " rotateY(180deg)",
+									"-webkit-transition": " #{settings.animationTime}ms",
+									"-moz-transition": " #{settings.animationTime}ms",
+									"-o-transition": " #{settings.animationTime}ms",
+									"transition": " #{settings.animationTime}ms",
+								}
+				return
+			
+			setInterval -> 
+				do flip
+				do settings.callback
+				return
+			, (settings.delay + settings.animationTime)
+			return
+		return
